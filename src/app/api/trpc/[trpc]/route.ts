eec6755f94f8becd840531934463c4f08b47afa3
Filip Sauer <sauer.filip@gmail.com>
2024-01-19 20:05:54 +0100

    try to fix deployment

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,18 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/server";
 
-const handler = (req: Request) =>
-  fetchRequestHandler({
+const handler = async (req: Request) =>
+  await fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
     createContext: () => ({}),
   });
 
-export { handler as GET, handler as POST };
+export async function GET(req: Request) {
+  return handler(req);
+}
+
+export async function POST(req: Request) {
+  return handler(req);
+}

e9c7bf51bb34dfd964cfd9c7a53562b16777595a
Filip Sauer <sauer.filip@gmail.com>
2024-01-22 03:58:46 +0100

    if poll not found, redirect to the main page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { serverClient } from "@/server/trpc";
 import { Poll } from "./_components/Poll";
+import { redirect } from "next/navigation";
 
 export default async function Page({
   params: { slug },
@@ -7,5 +8,6 @@ export default async function Page({
   params: { slug: string };
 }) {
   const poll = await serverClient.poll.get({ slug });
+  if (!poll) return redirect("/");
   return <Poll slug={slug} initialPoll={poll} />;
 }
